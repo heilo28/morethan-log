4405cfa9b1a709f66a90b929c744a2025f51532b
heilo28 <h.seoyoung28@gmail.com>
2023-09-02 21:39:57 +0900

    avtar 변경

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Hong Seoyoung",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "AI engineer",
     bio: "Test Page",
     email: "h.seoyoung28@gmail.com",
