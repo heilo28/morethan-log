d5937b4954df3d4a7f8d1805296cae1362f3b1fd
heilo28 <h.seoyoung28@gmail.com>
2023-09-02 17:31:33 +0900

    test

--- site.config.js
+++ site.config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     role: "AI engineer",
     bio: "Test Page",
     email: "h.seoyoung28@gmail.com",
-    linkedin: "morethanmin",
+    linkedin: "Seoyoung",
     github: "https://github.com/heilo28",
     instagram: "",
   },
@@ -18,12 +18,12 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "dev.seoyoung",
+    description: "welcome to dev.seoyoung!",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://seoyoung.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
